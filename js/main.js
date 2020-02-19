c4a85e051493c410117d61b6c6f8fb5f81453622
wshujin <54903336+wshujin@users.noreply.github.com>
2020-02-19 17:55:27 -0600

    Activity 5
    add my own data onto the map

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
 // Scriptsheet by Shujin Wang, 2020
+//declare map var in global scope
+var map;
+
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('mapid', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add Mapbox base tilelayer
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        accessToken: 'pk.eyJ1Ijoid3NodWppbiIsImEiOiJjaXQ0bTVwc3UwMDIzNmNxeGxmMTJpYTY4In0.FGW6oPGEWS2jdvROL4p7Ig'
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+//function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    $.getJSON("data/Airports.geojson", function(response){
+        //create marker options
+        var geojsonMarkerOptions = {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        //create a Leaflet GeoJSON layer and add it to the map
+        L.geoJson(response, {
+            onEachFeature: onEachFeature,
+            pointToLayer: function(feature, latlng){
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            }
+        }).addTo(map);
+    });
+};
+
+$(document).ready(createMap);
